2711fb3562770bcc64fbb103707982db1525a078
Isaque16 <120600603+Isaque16@users.noreply.github.com>
2024-11-23 14:41:25 -0500

    feat: alteração de senha e endereço

--- src/app/(user)/config/page.tsx
+++ src/app/(user)/config/page.tsx
@@ -1,12 +1,26 @@
 "use client";
 import IAddress from "@/interfaces/IAdress";
 import trpc from "@/trpc/client/trpc";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { setCookie, getCookie } from "cookies-next/client";
 import React, { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 export default function UserConfiguration() {
   const userId = getCookie("id");
   const { data, refetch } = trpc.customers.getById.useQuery(userId as string);
+
+  const [responseMessage, setResponseMessage] = useState(["", "", ""]);
+  useEffect(() => {
+    setTimeout(() => {
+      setResponseMessage((prev) => {
+        const newArray = [...prev];
+        newArray.fill("");
+        return newArray;
+      });
+    }, 5000);
+  }, [responseMessage]);
 
   // Preferência de Temas
   const [theme, setTheme] = useState("escuro");
@@ -22,16 +36,11 @@ export default function UserConfiguration() {
   }, []);
 
   // Alteração de Dados Pessoais
-  const { mutate: saveUserData } = trpc.customers.save.useMutation({
-    onSuccess() {
-      refetch();
-      setEditUserDataResponseMessage("");
-    }
+  const { mutateAsync: saveUserData } = trpc.customers.save.useMutation({
+    onSuccess: () => refetch()
   });
   const [usuario, setUsuario] = useState<string>(data?.nomeUsuario ?? "");
   const [email, setEmail] = useState<string>(data?.email ?? "");
-  const [editUserDataResponseMessage, setEditUserDataResponseMessage] =
-    useState<string>("");
 
   async function sendEditedUserData(newNomeUsuario: string, newEmail: string) {
     const newUserData = {
@@ -39,21 +48,136 @@ export default function UserConfiguration() {
       nomeUsuario: newNomeUsuario,
       email: newEmail
     };
-    saveUserData(newUserData);
+    await saveUserData(newUserData);
+
+    setResponseMessage((prev) => {
+      const newArray = [...prev];
+      newArray[0] = "Dados atualizados com sucesso";
+      return newArray;
+    });
   }
 
+  // Alteração de senha
+  const senhaAtual = data?.senha;
+  const [newSenha, setNewSenha] = useState("");
+  const [confirmNewSenha, setConfirmNewSenha] = useState("");
+  const [isVisible, setIsVisible] = useState(false);
+  const isValid =
+    newSenha != "" && senhaAtual !== newSenha && newSenha === confirmNewSenha;
+
+  const updateActualSenha = async () => {
+    if (isValid) {
+      await saveUserData({ ...data!, senha: newSenha });
+      setResponseMessage((prev) => {
+        const newArray = [...prev];
+        newArray[1] = "Senha atualizada com sucesso!";
+        return newArray;
+      });
+      setNewSenha("");
+      setConfirmNewSenha("");
+    } else {
+      setResponseMessage((prev) => {
+        const newArray = [...prev];
+        newArray[1] = "Senha inválida";
+        return newArray;
+      });
+      setNewSenha("");
+      setConfirmNewSenha("");
+    }
+  };
+
   // Gerenciamento de Endereços
+  const [showModal, setShowModal] = useState(false);
   const [endereco, setEndereco] = useState<IAddress>(
     data?.endereco ?? {
       CEP: "",
       estado: "",
+      cidade: "",
+      bairro: "",
       rua: "",
       numero: "",
-      bairro: "",
-      cidade: "",
       complemento: ""
     }
   );
+
+  const formDataSchema = z.object({
+    CEP: z.string(),
+    estado: z.string().min(3, "Precisa ter pelo menos 3 caracteres"),
+    cidade: z.string().min(3, "Precisa ter pelo menos 3 caracteres"),
+    bairro: z.string().min(6, "Precisa ter pelo menos 6 caracteres"),
+    rua: z.string().min(3, "Precisa ter pelo menos 3 caracteres"),
+    numero: z.string().min(1, "Deve ter pelo menos 11 dígitos"),
+    complemento: z.string().optional()
+  });
+
+  function EnderecoForm() {
+    const {
+      register,
+      handleSubmit,
+      reset,
+      formState: { errors }
+    } = useForm<IAddress>({
+      resolver: zodResolver(formDataSchema),
+      mode: "onChange",
+      defaultValues: endereco
+    });
+
+    useEffect(() => {
+      reset(endereco);
+    }, [endereco, reset]);
+
+    return (
+      <form
+        onSubmit={handleSubmit(async (adress) => {
+          await saveUserData({ ...data!, endereco: adress });
+          setResponseMessage((prev) => {
+            const newArray = [...prev];
+            newArray[2] = "Endereço atualizado com sucesso!";
+            return newArray;
+          });
+          setShowModal(false);
+        })}
+      >
+        {[
+          "CEP",
+          "estado",
+          "cidade",
+          "bairro",
+          "rua",
+          "numero",
+          "complemento"
+        ].map((field) => (
+          <div key={field}>
+            <label htmlFor={field} className="label">
+              {field.charAt(0).toUpperCase() + field.slice(1)}
+            </label>
+            <input
+              name={field}
+              type={field === "numero" ? "number" : "text"}
+              value={endereco[field as keyof IAddress] ?? ""}
+              placeholder={`Digite seu ${field}`}
+              onChange={(e) =>
+                setEndereco({ ...endereco, [field]: e.target.value })
+              }
+              {...register}
+              className="input input-bordered focus-within:ring-white focus-within:ring-2 w-full max-w-xs"
+            />
+            {errors[field as keyof IAddress] && (
+              <p className="text-error py-2 w-full max-w-xs">
+                {errors[field as keyof IAddress]?.message}
+              </p>
+            )}
+          </div>
+        ))}
+        <button
+          type="submit"
+          className="btn btn-success w-fit m-2 text-xl text-white mt-5"
+        >
+          Salvar endereço
+        </button>
+      </form>
+    );
+  }
 
   // Sincronização dos valores de `data` com os estados
   useEffect(() => {
@@ -145,8 +269,55 @@ export default function UserConfiguration() {
           >
             Salvar Alterações
           </button>
-          <p className="text-error empty:hidden">
-            {editUserDataResponseMessage}
+          <p className="text-error empty:hidden">{responseMessage[0]}</p>
+        </div>
+      </div>
+
+      <div className="card card-body card-bordered shadow-md mb-5 w-full md:w-1/2">
+        <h1 className="card-title">Alteração de Senha</h1>
+        <div className="flex flex-col gap-2">
+          <label className="label">Senha atual</label>
+          <div className="flex flex-row justify-between input input-bordered">
+            <input
+              type={isVisible ? "text" : "password"}
+              value={senhaAtual ?? ""}
+              readOnly={true}
+              className="input-disabled"
+            />
+            <button onClick={() => setIsVisible((value) => !value)}>
+              {isVisible ? "Ocultar" : "Mostrar"}
+            </button>
+          </div>
+
+          <label className="label">Nova senha</label>
+          <input
+            type="password"
+            className="input input-bordered"
+            placeholder="Nova Senha"
+            value={newSenha}
+            onChange={(e) => setNewSenha(e.target.value)}
+          />
+
+          <label className="label">Confirme a senha</label>
+          <input
+            type="password"
+            className="input input-bordered"
+            placeholder="Confirme a senha"
+            value={confirmNewSenha}
+            onChange={(e) => setConfirmNewSenha(e.target.value)}
+          />
+
+          <button onClick={updateActualSenha} className="btn btn-primary mt-2">
+            Salvar Alterações
+          </button>
+
+          <p
+            className={`${
+              isValid ? "text-success" : "text-error"
+            } mt-2 empty:hidden`}
+            aria-live="polite"
+          >
+            {responseMessage[1]}
           </p>
         </div>
       </div>
@@ -161,11 +332,23 @@ export default function UserConfiguration() {
           </p>
 
           {endereco ? (
-            <button className="btn btn-natural">Editar Endereço Atual</button>
+            <button
+              onClick={() => setShowModal((value) => !value)}
+              className="btn btn-natural"
+            >
+              Editar Endereço Atual
+            </button>
           ) : (
-            <button className="btn btn-secondary">Adicionar Endereço</button>
+            <button
+              onClick={() => setShowModal((value) => !value)}
+              className="btn btn-secondary"
+            >
+              Adicionar Endereço
+            </button>
           )}
+          <p className="text-success">{responseMessage[2]}</p>
         </div>
+        {showModal && <EnderecoForm />}
       </div>
     </main>
   );
